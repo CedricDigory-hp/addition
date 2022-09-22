29982f8e357d27dc2c1dc99762b487ae895bbae5
Malla Amruta <61353921+Amruta190@users.noreply.github.com>
2022-09-22 16:29:27 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,1 +1,14 @@
 //addition
+import java.util.*;
+class Add{
+  public static int add(int a,int b){
+    return a+b;
+  }
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    System.out.println(add(a,b));
+  }
+}
